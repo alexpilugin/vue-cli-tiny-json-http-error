dfe5b58f45da6e9f8f0d4c96efbd2a4545f7b147
Alex Pilugin <alexpilugin@users.noreply.github.com>
2018-02-12 11:33:29 +0000

    LoaderTiny-static-tiny-axios
    Replication of an error: Uncaught Error: Cannot find module 'http'

--- src/components/LoaderTiny.vue
+++ src/components/LoaderTiny.vue
@@ -21,30 +21,59 @@
 </template>
 
 <script>
-import tiny from 'tiny-json-http';
+import tiny from "tiny-json-http";
+import axios from 'axios';
 
 export default {
-  name: 'LoaderTiny',
-  data () {
+  name: "LoaderTiny",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: "Welcome to Your Vue.js App",
       posts: [],
       errors: []
-    }
+    };
   },
   methods: {
-    getData: function (event) {
-		  const reqURL = `http://jsonplaceholder.typicode.com/posts`
-			tiny.get( {reqURL} ).then(response => {
-				// JSON responses are automatically parsed.
+    getData: function(event) {
+      const reqURL = `http://jsonplaceholder.typicode.com/posts`;
+
+      /*
+      this.posts = [
+        {
+          userId: 1,
+          id: 1,
+          title:
+            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          body:
+            "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "qui est esse",
+          body:
+            "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        }
+      ];
+      */
+
+      /*
+      tiny.get( {reqURL} ).then(response => {
+        // JSON responses are automatically parsed.
+        console.log(response);
         this.posts = response.body.Items;
-        console.log(this.posts);
-			}).catch(err => {
-				this.errors.push(err)
-			})
+      }).catch(err =>  this.errors.push(err))
+      */
+
+      axios.get(reqURL)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.posts = response.data;
+        })
+        .catch(e =>  this.errors.push(e))
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -61,7 +90,7 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
+a a {
   color: #42b983;
 }
 </style>
